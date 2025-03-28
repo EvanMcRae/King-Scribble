93b3ca1a6ab82bb0e463f8653958d095d3d8e6d2
EvanMcRae <evanmmcrae@gmail.com>
2025-03-28 16:47:34 -0400

    Try that one more time

--- Assets/Scripts/Management/ChangeScene.cs
+++ Assets/Scripts/Management/ChangeScene.cs
@@ -22,6 +22,7 @@ public class ChangeScene : MonoBehaviour
         ScreenWipe.instance.WipeIn();
         ScreenWipe.PostUnwipe += () => { changingScene = false; };
         yield return new WaitForSeconds(1f);
+        PlayerController.instance.KillTweens();
         EventSystem eventSystem = FindObjectOfType<EventSystem>();
         Destroy(eventSystem?.gameObject);
         SceneHelper.LoadScene(scene);
