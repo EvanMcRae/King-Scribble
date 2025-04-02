b2122726bda75d6fc81fd6ae5ed3461c72cf1a56
EvanMcRae <evanmmcrae@gmail.com>
2025-04-02 16:53:36 -0400

    I thought I already fixed you >:(

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -88,7 +88,6 @@ public class PlayerController : MonoBehaviour
             {
                 DOTween.To(() => virtualCamera.m_Lens.OrthographicSize, x => virtualCamera.m_Lens.OrthographicSize = x, levelZoom, 1f);
                 isSprintMoving = false;
-                transform.DOMove(new Vector3(0, 0, 1), 1).SetEase(Ease.OutElastic);
             }
             isSprinting = false;
             sprintSpeedMultiplier = 1f;
