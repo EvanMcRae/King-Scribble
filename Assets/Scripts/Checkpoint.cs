5062f9c7a72f9d3ec913fb05beff8d0000f6cf1f
EvanMcRae <evanmmcrae@gmail.com>
2025-04-08 22:48:43 -0400

    Checkpoint sounds

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -7,14 +7,30 @@ public class Checkpoint : MonoBehaviour
     public Sprite flagpole;
     public Sprite with_flag; // Temp - will rewrite to animate raising/lowering later
     private bool has_triggered = false;
+    public SoundPlayer soundPlayer;
+
+    private void Awake()
+    {
+        if (PlayerVars.instance.GetSpawnPos() == transform.position)
+        {
+            ActivateCheckpoint();
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !has_triggered)
         {
-            gameObject.GetComponent<SpriteRenderer>().sprite = with_flag;
-            PlayerVars.instance.MaxDoodleFuel();
-            PlayerVars.instance.SetSpawnPos(transform.position);
-            has_triggered = true;
+            ActivateCheckpoint();
+            soundPlayer.PlaySound("Level.Checkpoint");
         }
+    }
+
+    void ActivateCheckpoint()
+    {
+        GetComponent<SpriteRenderer>().sprite = with_flag;
+        PlayerVars.instance.MaxDoodleFuel();
+        PlayerVars.instance.SetSpawnPos(transform.position);
+        has_triggered = true;
     }
 }
