f7218a9bea1d5556931faa203cd3c6dc2cc73870
EvanMcRae <evanmmcrae@gmail.com>
2025-05-10 19:10:41 -0400

    More attempted tweaks

--- Assets/Scripts/UI/ImageOscillator.cs
+++ Assets/Scripts/UI/ImageOscillator.cs
@@ -6,12 +6,17 @@ public class ImageOscillator : MonoBehaviour
 {
     [SerializeField] private float amplitude = 0, period = 0;
     [SerializeField] private bool local = true;
-    
+    [SerializeField] private Vector3 pos;
+
+    private void Start()
+    {
+        pos = local ? transform.localPosition : transform.position;
+    }
+
     void Update()
     {
         if (!GameManager.paused)
         {
-            Vector3 pos = local ? transform.localPosition : transform.position;
             pos.y += amplitude * Mathf.Sin(Time.time * period * 2 * Mathf.PI);
             if (local)
                 transform.localPosition = pos;
