583f8ad893d3ae754224666a793ed93717b932d7
EvanMcRae <evanmmcrae@gmail.com>
2025-05-10 18:23:40 -0400

    Add screenshot manager, oscillating tool items

---
+++ Assets/Scripts/Management/ScreenshotManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class ScreenshotManager : MonoBehaviour
+{
+    private static bool initialized = false;
+    private static int numScreenshots;
+
+    // Use this for initialization
+    void Awake()
+    {
+        string path = Path.Combine(Application.persistentDataPath, "Screenshots");
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+        var screenshots = Directory.EnumerateFiles(path);
+        foreach (string _ in screenshots)
+        {
+            numScreenshots++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            ScreenCapture.CaptureScreenshot(Path.Combine(Application.persistentDataPath, "Screenshots", numScreenshots + ".png"));
+            numScreenshots++;
+        }
+
+    }
+}
--- Assets/Scripts/UI/ImageOscillator.cs
+++ Assets/Scripts/UI/ImageOscillator.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class ImageOscillator : MonoBehaviour
 {
     [SerializeField] private float amplitude = 0, period = 0;
+    [SerializeField] private bool local = true;
     
     void Update()
     {
-        Vector3 pos = transform.localPosition;
-        pos.y += amplitude * Mathf.Sin(Time.time * period * 2 * Mathf.PI);
-        transform.localPosition = pos;
+        if (!GameManager.paused)
+        {
+            Vector3 pos = local ? transform.localPosition : transform.position;
+            pos.y += amplitude * Mathf.Sin(Time.time * period * 2 * Mathf.PI);
+            if (local)
+                transform.localPosition = pos;
+            else
+                transform.position = pos;
+        }
     }
 }
