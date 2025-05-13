32592dff4b79d923ef17d52c2ec2a7c29480c99a
EvanMcRae <evanmmcrae@gmail.com>
2025-05-12 20:42:52 -0400

    Implement new hints + shorten reset time

--- Assets/Scripts/Management/GameManager.cs
+++ Assets/Scripts/Management/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
     public static Action ResetAction;
     public ResetPrompt resetPrompt;
     public float resetTime;
-    public const float maxResetTime = 2f;
+    public const float maxResetTime = 1.5f;
 
     // Start is called before the first frame update
     void Start()
