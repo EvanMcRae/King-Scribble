e0c5312acabd516a096ecaeefabc801b430e91e2
EvanMcRae <evanmmcrae@gmail.com>
2025-05-13 03:34:23 -0400

    Support mac shortcut for toggling fullscreen

--- Assets/Scripts/Management/ScreenshotManager.cs
+++ Assets/Scripts/Management/ScreenshotManager.cs
@@ -28,17 +28,30 @@ public class ScreenshotManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Take screenshots
         if (Input.GetKeyDown(KeyCode.F2))
         {
             ScreenCapture.CaptureScreenshot(Path.Combine(Application.persistentDataPath, "Screenshots", numScreenshots + ".png"));
             numScreenshots++;
         }
 
+        // Toggle full screen
+#if UNITY_STANDALONE_OSX
+        if (Input.GetKey(KeyCode.LeftCommand) && Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.F))
+        {
+            SettingsManager.currentSettings.fullScreen = !SettingsManager.currentSettings.fullScreen;
+            Screen.SetResolution(Display.main.systemWidth, (int)(9 / 16f * Display.main.systemWidth), SettingsManager.currentSettings.fullScreen);
+            ToggleFullScreen?.Invoke();
+        }
+#endif
+
+#if UNITY_STANDALONE_WIN
         if (Input.GetKeyDown(KeyCode.F11))
         {
             SettingsManager.currentSettings.fullScreen = !SettingsManager.currentSettings.fullScreen;
             Screen.SetResolution(Display.main.systemWidth, (int)(9 / 16f * Display.main.systemWidth), SettingsManager.currentSettings.fullScreen);
             ToggleFullScreen?.Invoke();
         }
+#endif
     }
 }
