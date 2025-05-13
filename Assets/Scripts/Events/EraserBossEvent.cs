69bc7d79c0ba2f03753333c90fa20e89b3b06e8d
EvanMcRae <evanmmcrae@gmail.com>
2025-05-13 18:00:48 -0400

    Fix the button and add some EB sound effects

--- Assets/Scripts/Events/EraserBossEvent.cs
+++ Assets/Scripts/Events/EraserBossEvent.cs
@@ -14,6 +14,8 @@ public class EraserBossEvent : MonoBehaviour
     private SpriteRenderer buttonTop;
     private Color purpleColor;
     [SerializeField] private GameObject buttonParticles;
+    [SerializeField] private BoxCollider2D buttonBound;
+    [SerializeField] private Vector2 offset1, size1, offset2, size2;
 
     void Start()
     {
@@ -45,7 +47,11 @@ public class EraserBossEvent : MonoBehaviour
 
     public void Activate() {
         isButtonPressed = true;
-        if(isButtonActive) {
+        Debug.Log("activate");
+        buttonBound.offset = offset2;
+        buttonBound.size = size2;
+        if (isButtonActive) {
+            soundPlayer.PlaySound("EraserBoss.ButtonActivate");
             left.Activate();
             right.Activate();
         }
@@ -53,7 +59,10 @@ public class EraserBossEvent : MonoBehaviour
 
     public void Deactivate() {
         isButtonPressed = false;
-        if(isButtonActive) {
+        Debug.Log("deactivate");
+        buttonBound.offset = offset1;
+        buttonBound.size = size1;
+        if (isButtonActive) {
             left.Deactivate();
             right.Deactivate();
         }
