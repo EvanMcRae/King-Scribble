1a60e2b90bf88cb1a4e953fc1b2f64d962b2d9b0
EvanMcRae <evanmmcrae@gmail.com>
2025-05-15 14:10:42 -0400

    Make banner come out from the top instead

--- Assets/Scripts/UI/LevelStartBanner.cs
+++ Assets/Scripts/UI/LevelStartBanner.cs
@@ -10,16 +10,18 @@ public class LevelStartBanner : MonoBehaviour
     [SerializeField] private RectTransform active;
     [SerializeField] private RectTransform end;
     RectTransform rectTransform;
+    private bool needsStartPos = false;
     
     void Start()
     {
        rectTransform = GetComponent<RectTransform>();
+       if (needsStartPos)
+            rectTransform.position = new Vector3(rectTransform.position.x, active.position.y);
     }
 
     public void setStartPosition() {
         Debug.Log("setting banner pos");
-        rectTransform = GetComponent<RectTransform>();
-        rectTransform.position = new Vector3(active.position.x, rectTransform.position.y); 
+        needsStartPos = true;
     }
 
     public void PlayLevelStartAnimation() {
@@ -30,8 +32,8 @@ public class LevelStartBanner : MonoBehaviour
 
     // Update is called once per frame
     private IEnumerator StartAnimation() {
-        //transform.DOMoveX(active.position.x, 0.5f);
+        //transform.DOMoveY(active.position., 0.5f);
         yield return new WaitForSeconds(2.0f);
-        transform.DOMoveX(end.position.x, 0.8f);
+        transform.DOMoveY(end.position.y, 0.8f);
     }
 }
