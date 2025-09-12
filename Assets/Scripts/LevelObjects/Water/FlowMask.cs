cddbc1f95224c212de766e0fd53967c4db96ee67
EvanMcRae <evanmmcrae@gmail.com>
2025-09-12 02:07:31 -0400

    Implement water flow mask

---
+++ Assets/Scripts/LevelObjects/Water/FlowMask.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+[RequireComponent(typeof(SpriteMask))]
+[RequireComponent(typeof(ParentConstraint))]
+public class FlowMask : MonoBehaviour
+{
+    [SerializeField] private InteractableWater water;
+    private SpriteMask spriteMask;
+    private SpriteRenderer spriteRenderer;
+    private Sprite sprite;
+    public Vector2Int fudgeFactor = new(5, 3);
+    private bool queueComputeGeometry = false;
+
+    void Awake()
+    {
+        spriteMask = GetComponent<SpriteMask>();
+        var texture2D = new Texture2D((int)water.Width + fudgeFactor.x, (int)water.Height + fudgeFactor.y);
+        sprite = Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), 0.5f * Vector2.one, 1);
+        spriteMask.sprite = sprite;
+        water.MaskUpdateAction += QueueComputeGeometry;
+        GetComponent<ParentConstraint>().SetTranslationOffset(0, new Vector3(fudgeFactor.x / 2.0f, fudgeFactor.y / 2.0f));
+    }
+
+    void QueueComputeGeometry()
+    {
+        queueComputeGeometry = true;
+    }
+
+    void Update()
+    {
+        if (queueComputeGeometry)
+        {
+            ComputeGeometry();
+            queueComputeGeometry = false;
+        }
+    }
+
+    void ComputeGeometry()
+    {
+        Mesh mesh = water.WaterMesh;
+
+        // Compute mesh bounds in 2D to normalize into Sprite.rect
+        Bounds bounds = mesh.bounds;
+        Vector2 meshMin = new(bounds.min.x, bounds.min.y);
+        Vector2 meshSize = new(bounds.size.x, bounds.size.y);
+        Vector2 spriteSize = sprite.rect.size;
+
+        Vector2[] vertices = Utils.ConvertArray(mesh.vertices); ;
+
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            // Normalize vertex within mesh bounds (0 to 1)
+            Vector2 normalized = new(
+                (vertices[i].x - meshMin.x) / meshSize.x,
+                (vertices[i].y - meshMin.y) / meshSize.y
+            );
+
+            // Scale to sprite rect size in pixels
+            vertices[i] = new(
+                normalized.x * ((int)water.Width), // TODO this will do
+                normalized.y * ((int)water.Height)
+            );
+        }
+
+        ushort[] triangles = new ushort[mesh.triangles.Length];
+        for (int i = 0; i < mesh.triangles.Length; i++)
+        {
+            triangles[i] = (ushort)mesh.triangles[i];
+        }
+        sprite.OverrideGeometry(vertices, triangles);
+    }
+}
--- Assets/Scripts/Management/Utils.cs
+++ Assets/Scripts/Management/Utils.cs
@@ -6,6 +6,16 @@ public static class Utils
     // TODO: Disable this for actual builds!!!
     public const bool CHEATS_ENABLED = true;
 
+    public static Vector2[] ConvertArray(Vector3[] v3)
+    {
+        Vector2[] v2 = new Vector2[v3.Length];
+        for (int i = 0; i < v3.Length; i++)
+        {
+            v2[i] = v3[i];
+        }
+        return v2;
+    }
+
     // Functions in this class are accessible by all scripts in the current folder
     public static RaycastHit2D[] RaycastAll(Camera maincamera, Vector2 screenPosition, int layermask, float radius)
     {
